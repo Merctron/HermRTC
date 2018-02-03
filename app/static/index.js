0d6a0f507c34d1d4e11accef61d8ab34d261ca91
Merctron <murtuzakainan@gmail.com>
2018-02-03 12:41:58 -0500

    Initial Commit

---
+++ app/static/index.js
@@ -0,0 +1,21 @@
+var video_out = document.getElementById("vid-box");
+
+function login(form) {
+	var phone = window.phone = PHONE({
+	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+	    publish_key   : 'pub-c-2bde573c-f04c-41ac-92bb-9ce801e8a362',
+	    subscribe_key : 'sub-c-b9c4c050-08fb-11e8-b7c9-024a5d295ade',
+	});	
+	phone.ready(function(){ form.username.style.background="#55ff5b"; });
+	phone.receive(function(session){
+	    session.connected(function(session) { video_out.appendChild(session.video); });
+	    session.ended(function(session) { video_out.innerHTML=''; });
+	});
+	return false; 	// So the form does not submit.
+}
+
+function makeCall(form){
+	if (!window.phone) alert("Login First!");
+	else phone.dial(form.number.value);
+	return false;
+}
